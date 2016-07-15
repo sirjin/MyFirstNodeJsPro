236666c215c92d3a0ef26158958a614e2e5c2c0a
fengjin <sirjin@yeah.net>
2016-07-15 09:47:15 +0800

    添加样式

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -6,10 +6,10 @@ function loginCheck() {
     var _userName = $("#username").val();
     var _password = $("#password").val();
     if(_userName==""){
-        $("#username_span").css({"color":"red"}).text("用户名不得为空!");
+        $("#username_span").css({"color":"red","font-size":"10px"}).text("用户名不得为空!");
         return false;
     }else if(_password==""){
-        $("#password_span").css({"color":"red"}).text("用户密码不得为空!");
+        $("#password_span").css({"color":"red","font-size":"10px"}).text("用户密码不得为空!");
         return false;
     }else{
         return true;
