03609c5d21b96ac3bee9985a84735fd069687163
fengjin <sirjin@yeah.net>
2016-07-22 13:52:37 +0800

    添加log4js模块，（学习:该模块添加到本项目中，所以在项目的node_modules文件夹下面能看到；疑问:问什么这次修改package.json文件，它不帮我自动添加modules了？）

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
-var logger = require('morgan');
+//var logger = require('morgan');
 var session = require('express-session');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 //var mongoose = require('mongoose');
 var MongoStore = require('connect-mongo');
 var settings = require('./settings');
+var log4js = require('log4js');
+//log4js相关配置
+log4js.configure({
+  appenders: [
+    { type: 'console' }, //控制台输出
+    {
+      type: 'file', //文件输出
+      filename: 'logs/access.log',
+      maxLogSize: 1024,
+      backups:3,
+      category: 'normal'
+    }
+  ]
+});
+
+var logger = log4js.getLogger('normal');
+logger.setLevel('INFO');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -21,7 +38,8 @@ app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+//app.use(logger('dev'));
+app.use(log4js.connectLogger(logger, {level:log4js.levels.INFO}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
